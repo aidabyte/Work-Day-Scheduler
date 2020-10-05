67f380e9ad004fe782e80abf845ec734b692e14e
aidabyte <ablinstrubyte@gmail.com>
2020-10-04 19:44:55 -0500

    had to fix time from html to script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var currentDate = ("#currentDay")
 var userInputArray = []
 var time = (".input-time")
 
-
+// making sure that jquery is working
 $(document).ready(function() {
     $(".saveBtn").on("click", function() {
 
@@ -11,9 +11,29 @@ $(document).ready(function() {
         .val();
 
         var time = $(this).attr("input-time");
-        var hour = moment().hout();
+        var hour = moment().hour();
 
         localStorage.setItem(time,value);
         
     });
+    //date,time, and updating seconds every one thousand miliseconds
+    $("currentDay").text(moment());
+    setInterval(displayTodaysDateAndTime, 1000)
+
+    currentDate = $("#currentDay");
+    displayTodaysDateAndTime();
+    
+    function displayTodaysDateAndTime() {
+        currentDate.text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+    }
 });
+
+    userInputArray = [];
+for( let i = 9; i < userInputArray.length; i ++){
+    userInputArray.push(
+        moment()
+        .hour(i)
+        .format("h, a")
+    );
+
+}
